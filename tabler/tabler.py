46ce3fd3acb51d7bfe9f886f00bc709f3501d9fd
Ben Schmeckpeper <ben.schmeckpeper@gmail.com>
2013-11-22 21:57:26 -0600

    Capture rows from the table using the callbacks.

--- tabler/tabler.py
+++ tabler/tabler.py
@@ -3,8 +3,20 @@ from parser import TableParser
 class Tabler:
     def __init__(self, html):
         self._html = html
-        self.parser = TableParser()
+        self.header = []
+        self.body = []
+        self.footer = []
+        self.parser = TableParser(self.add_header_row, self.add_body_row, self.add_footer_row)
         self.parser.feed(html)
-
+        
     def rows(self):
-        return []
+        return self.header + self.body + self.footer
+
+    def add_header_row(self, cells):
+        self.header.append(cells)
+
+    def add_body_row(self, cells):
+        self.body.append(cells)
+
+    def add_footer_row(self, cells):
+        self.footer.append(cells)
