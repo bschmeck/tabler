c83c63a6b1ff1cc6d6d4f71f2da3affbb167738d
Ben Schmeckpeper <ben@aisle50.com>
2013-11-25 09:31:03 -0600

    Use a class to store row data, and allow lookup via index or header cell value.

--- tabler/tabler.py
+++ tabler/tabler.py
@@ -12,11 +12,35 @@ class Tabler:
     def rows(self):
         return self.header + self.body + self.footer
 
+    def body_rows(self):
+        return self.body
+
     def add_header_row(self, cells):
-        self.header.append(cells)
+        self.header.append(self.Row(self, cells))
 
     def add_body_row(self, cells):
-        self.body.append(cells)
+        self.body.append(self.Row(self, cells))
 
     def add_footer_row(self, cells):
-        self.footer.append(cells)
+        self.footer.append(self.Row(self, cells))
+
+    def index_of(self, index):
+        if isinstance(index, str):
+            if len(self.header) > 0:
+                try:
+                    return self.header[0].index(index)
+                except ValueError:
+                    raise ValueError(index + " is not a valid index value.")
+            raise ValueError(index + " is not a valid index value.")
+        return index
+
+    class Row:
+        def __init__(self, tabler, cells):
+            self._tabler = tabler
+            self._cells = cells
+
+        def __getitem__(self, index):
+            return self._cells[self._tabler.index_of(index)]
+
+        def index(self, elt):
+            return self._cells.index(elt)
